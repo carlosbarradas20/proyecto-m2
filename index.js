db38a74a4aa725759f335378b13b1a788d883724
carlosbarradas20 <carlosbarradasaleman30@gmail.com>
2023-07-25 14:40:27 -0600

    entrega

--- index.js
+++ index.js
@@ -1,38 +1,54 @@
 
-var inicioDiv = document.createElement("div");
+let inicioDiv = document.createElement("div");
 inicioDiv.setAttribute("id", "inicio");
 
-var header = document.querySelector("header.header");
-header.insertAdjacentElement("afterend", inicioDiv);
 
-localStorage.setItem("empresa", "Inmobiliaria Metros Cuadrados");
-
-let empresa = localStorage.getItem("empresa");
-console.log("Empresa:", empresa);
-
-// // Eventos
-// var navLinks = document.querySelectorAll(".nav-link");
-
-// navLinks.forEach(function(link) {
-//   link.addEventListener("click", function(event) {
-//     event.preventDefault();
-//     var href = this.getAttribute("href");
-//     console.log("Has hecho clic en:", href="inmobiliaria metros cuadrados");
-//   });
-// });
-
-
-var inicioDiv = document.createElement("div");
-inicioDiv.setAttribute("id", "inicio");
-
-var mensajeInicio = document.createElement("h1");
 mensajeInicio.textContent = "Ciudades en las que estamos";
-
-
 mensajeInicio.style.color = "#000000";
 mensajeInicio.style.textAlign = "center";
-
 inicioDiv.appendChild(mensajeInicio);
 
-var header = document.querySelector("header.header");
+
+let header = document.querySelector("header.header");
 header.insertAdjacentElement("afterend", inicioDiv);
+
+
+let seccion2 = document.querySelector(".section2");
+
+
+async function cargarImagen(src) {
+  try {
+    const img = new Image();
+    await new Promise((resolve, reject) => {
+      img.onload = resolve;
+      img.onerror = reject;
+      img.src = src;
+    });
+    return img;
+  } catch (error) {
+    console.error("Error al cargar imagen:", error.message);
+  }
+}
+
+
+ciudades.forEach(async (ciudad) => {
+  const article = document.createElement("article");
+  article.className = "postcard dark blue";
+
+  const link = document.createElement("a");
+  link.className = "postcard__img_link";
+  link.href = `./secciones/${ciudad.nombre.toLowerCase()}.html`;
+
+  try {
+    const img = await cargarImagen(ciudad.imagenSrc);
+    img.className = "postcard__img";
+    img.alt = "Image Title";
+    link.appendChild(img);
+  } catch (error) {
+    console.error("Error al cargar imagen:", error.message);
+  }
+
+  article.appendChild(link);
+  seccion2.appendChild(article);
+});
+
